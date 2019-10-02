805a995c6041ffc7688673c738ded4cb957024c2
pmapelli <pmapelli@hotmail.com>
2019-10-02 17:08:17 -0300

    feat: Styling main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello word</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repósitorio" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
